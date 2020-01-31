573d1c952e11e9711dedf11285e32f89c8fe879b
unknown <razvanbunga@gmail.com>
2020-01-31 22:04:53 +0000

    audio

--- python/main.py
+++ python/main.py
@@ -1,16 +1,15 @@
-import os
-import subprocess
+import os, shutil, subprocess
 
 
 def flac2wav(flac_path):
     flacs = os.listdir(flac_path)
 
-
     for flac in flacs:
         flac = flac.split('.')[0]
         print(flac)
 
-        subprocess.run('ffmpeg -i {0}/{1}.flac {0}/{1}.ogg'.format(flac_path, flac))
+        subprocess.run('ffmpeg -i {0}/{1}.ogg {0}/{1}.mp3 -n'.format(flac_path, flac))
+
 
 if __name__ == "__main__":
     path = os.path.dirname('./../src/components/flac/')
